c400da7b2b3e7c1a2cb519afd5cda2d5f8892948
gambada <daniele.gambaletta@studio.unibo.it>
2023-06-11 21:39:52 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,86 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <SPI.h>               // Built-in 
-#include "EPD_WaveShare.h"     // Copyright (c) 2017 by Daniel Eichhorn https://github.com/ThingPulse/minigrafx
-#include "EPD_WaveShare_42.h"  // Copyright (c) 2017 by Daniel Eichhorn https://github.com/ThingPulse/minigrafx
-#include "MiniGrafx.h"         // Copyright (c) 2017 by Daniel Eichhorn https://github.com/ThingPulse/minigrafx
-#include "DisplayDriver.h"     // Copyright (c) 2017 by Daniel Eichhorn https://github.com/ThingPulse/minigrafx
-#include "qrcode.h"            // Copyright (c) //https://github.com/ricmoo/qrcode/
+/** 
+ *  TaskScheduler Test
+ *
+ *  Initially only tasks 1 and 2 are enabled
+ *  Task1 runs every 2 seconds 10 times and then stops
+ *  Task2 runs every 3 seconds indefinitely
+ *  Task1 enables Task3 at its first run
+ *  Task3 run every 5 seconds
+ *  Task1 disables Task3 on its last iteration and changed Task2 to run every 1/2 seconds
+ *  At the end Task2 is the only task running every 1/2 seconds
+ */
+ 
+ 
+#include <TaskScheduler.h>
 
-#define SCREEN_WIDTH  400.0    // Set for landscape mode, don't remove the decimal place!
-#define SCREEN_HEIGHT 300.0
-#define BITS_PER_PIXEL 1
-#define EPD_BLACK 0
-#define EPD_WHITE 1
-uint16_t palette[] = { 0, 1 };
+// Callback methods prototypes
+void t1Callback();
+void t2Callback();
+void t3Callback();
 
-// pins_arduino.h, e.g. LOLIN32 D322
-static const uint8_t EPD_BUSY = 34;
-static const uint8_t EPD_SS   = 5;
-static const uint8_t EPD_RST  = 33;
-static const uint8_t EPD_DC   = 32;
-static const uint8_t EPD_SCK  = 18;
-static const uint8_t EPD_MISO = 19; // Master-In Slave-Out not used, as no data from display
-static const uint8_t EPD_MOSI = 23;
+//Tasks
+Task t4();
+Task t1(2000, 10, &t1Callback);
+Task t2(3000, TASK_FOREVER, &t2Callback);
+Task t3(5000, TASK_FOREVER, &t3Callback);
 
-EPD_WaveShare42 epd(EPD_SS, EPD_RST, EPD_DC, EPD_BUSY);
-MiniGrafx gfx = MiniGrafx(&epd, BITS_PER_PIXEL, palette);
+Scheduler runner;
 
 
-QRCode qrcode;
-
-
-//#########################################################################################
-void Display_QRcode(int offset_x, int offset_y, int element_size, int QRsize, int ECC_Mode, const char* Message){
-  // QRcode capacity examples Size-12  65 x 65 LOW      883 535 367 
-  //                                           MEDIUM   691 419 287 
-  //                                           QUARTILE 489 296 203 
-  //                                           HIGH     374 227 155 
-  uint8_t qrcodeData[qrcode_getBufferSize(QRsize)];
-  //ECC_LOW, ECC_MEDIUM, ECC_QUARTILE and ECC_HIGH. Higher levels of error correction sacrifice data capacity, but ensure damaged codes remain readable.
-  if (ECC_Mode%4 == 0) qrcode_initText(&qrcode, qrcodeData, QRsize, ECC_LOW, Message);
-  if (ECC_Mode%4 == 1) qrcode_initText(&qrcode, qrcodeData, QRsize, ECC_MEDIUM, Message);
-  if (ECC_Mode%4 == 2) qrcode_initText(&qrcode, qrcodeData, QRsize, ECC_QUARTILE, Message);
-  if (ECC_Mode%4 == 3) qrcode_initText(&qrcode, qrcodeData, QRsize, ECC_HIGH, Message);
-  for (int y = 0; y < qrcode.size; y++) {
-    for (int x = 0; x < qrcode.size; x++) {
-      if (qrcode_getModule(&qrcode, x, y)) {
-        gfx.setColor(EPD_BLACK);
-        gfx.fillRect(x*element_size+offset_x,y*element_size+offset_y,element_size,element_size);
-        
-      }
-      else 
-      {
-        gfx.setColor(EPD_WHITE);
-        gfx.fillRect(x*element_size+offset_x,y*element_size+offset_y,element_size,element_size);
-      }
+void t1Callback() {
+    Serial.print("t1: ");
+    Serial.println(millis());
+    
+    if (t1.isFirstIteration()) {
+      runner.addTask(t3);
+      t3.enable();
+      Serial.println("t1: enabled t3 and added to the chain");
     }
-  }
-}
-//#########################################################################################
-void Clear_Screen(){
-  gfx.fillBuffer(EPD_WHITE);
-  gfx.commit();
-  delay(4000);
-}
-
-
-//#########################################################################################
-void setup() {
-  Serial.begin(115200);
-  gfx.init();
-  gfx.setRotation(1);
-  gfx.fillBuffer(EPD_WHITE);
+    
+    if (t1.isLastIteration()) {
+      t3.disable();
+      runner.deleteTask(t3);
+      t2.setInterval(500);
+      Serial.println("t1: disable t3 and delete it from the chain. t2 interval set to 500");
+    }
 }
 
-//#########################################################################################
-void loop() {
-  //Display_QRcode(x,y,element_size,QRcode Size,Error Detection/Correction Level,"string for encoding");
-  gfx.drawString(110,10,"San Vitale");
-  gfx.drawString(20,40,"Temperatura: 40°C | Umidità: troppa");
-  gfx.drawString(5,70,"Qulità dell'aria: pessima | Pressione: alta");
+void t2Callback() {
+    Serial.print("t2: ");
+    Serial.println(millis());
   
-  Serial.print("ciao");
+}
 
-  Display_QRcode(30,130,2,26,1,"0000001111111122222222223344555556600000011111111222222222233445555566000000111111112222222222334455555660000001111111122222222223344555556600000011111111222222222233445555566");
-  gfx.commit();
-  delay(1000000); // Delay before we do it again
-  Clear_Screen();
+void t3Callback() {
+    Serial.print("t3: ");
+    Serial.println(millis());
+  
+}
+
+void setup () {
+  Serial.begin(115200);
+  Serial.println("Scheduler TEST");
+  
+  runner.init();
+  Serial.println("Initialized scheduler");
+  
+  runner.addTask(t1);
+  Serial.println("added t1");
+  
+  runner.addTask(t2);
+  Serial.println("added t2");
+
+  delay(5000);
+  
+  t1.enable();
+  Serial.println("Enabled t1");
+  t2.enable();
+  Serial.println("Enabled t2");
+}
+
+
+void loop () {
+  runner.execute();
 }
