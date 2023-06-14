b48b58db398ca32b27cb1a925738e93c81b4c5e7
gambada <daniele.gambaletta@studio.unibo.it>
2023-06-14 18:42:36 +0200

    update qrcode

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#include "Arduino.h"
+
+// Configuration file (pins, periods...)
+
+#endif
---
+++ src/strings.h
@@ -0,0 +1,8 @@
+#ifndef __STRINGS__
+#define __STRINGS__
+
+#include "Arduino.h"
+
+// Strings file
+
+#endif
